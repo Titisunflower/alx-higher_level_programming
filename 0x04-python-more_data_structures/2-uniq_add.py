4ce2a33170333772d8fe1f14823f62ea05064e9d
Titisunflower <henuwz@gmail.com>
2023-03-15 05:50:42 -0700

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    maxval = 0
-    maxkey = None
-    for k, v in a_dictionary.items():
-        if v > maxval:
-            maxval = v
-            maxkey = k
-    return maxkey
+def uniq_add(my_list=[]):
+    return sum(set(my_list))

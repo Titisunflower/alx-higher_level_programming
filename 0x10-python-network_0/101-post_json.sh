cd7f6a25680f80a9fae9b8557965574b223e2dc3
Titisunflower <henuwz@gmail.com>
2023-06-30 04:27:30 -0700

    Adv

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends file as post data
-curl -s -X POST -d @"$2" -H "Content-Type: application/json" "$1"i
+# Bash script sends JSON POST request to URL as 1st arg & disp body response
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"

b8ba46658d1e499534a4a46d6c4f1befe8dbf852
Titisunflower <henuwz@gmail.com>
2023-06-30 04:43:16 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" Fetches basic request with urlib"""
-from urllib import request
-
-with request.urlopen("https://intranet.hbtn.io/status") as response:
-    r = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(r), r, r.decode('utf-8')))
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
